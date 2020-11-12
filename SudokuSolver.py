245b2943d237dce0e761025832f328bee3d21150
Henry Lin <hlin@trinity.edu>
2020-11-12 22:14:34 +0800

    minor bug fixes

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -5,7 +5,7 @@ def findEmpty(board):
 
     for x in range(9):
         for y in range(9):
-            if board[x][y] == -1:
+            if board[x][y] == 0:
                 return x,y
     
     return None, None
@@ -47,25 +47,23 @@ def solve(board):
             if solve(board):
                 return True
 
-        board[x][y] = -1
+        board[x][y] = 0
 
     return False
 
 
 
-if __name__ == '__main__':
-    example_board = [
-        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
-        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
-        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
-
-        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
-        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
-        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
-
-        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
-        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
-        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
-    ]
-    print(solve(example_board))
-    print(example_board)
+# if __name__ == '__main__':
+#     example_board = [
+#         [7,8,0,4,0,0,1,2,0],
+#         [6,0,0,0,7,5,0,0,9],
+#         [0,0,0,6,0,1,0,7,8],
+#         [0,0,7,0,4,0,2,6,0],
+#         [0,0,1,0,5,0,9,3,0],
+#         [9,0,4,0,6,0,0,0,5],
+#         [0,7,0,3,0,0,0,1,2],
+#         [1,2,0,0,0,7,4,0,0],
+#         [0,4,9,2,0,6,0,0,7]
+#     ]
+#     print(solve(example_board))
+#     print(example_board)
