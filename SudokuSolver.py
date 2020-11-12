4a44286c9017874b0d43b9b6b1ee651a454dc55b
Henry Lin <hlin@trinity.edu>
2020-11-12 12:12:26 +0800

    a few bug fixes

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -11,29 +11,25 @@ def findEmpty(board):
     return None, None
 
 
-def isValid(board, x, y, guess):
+def isValid(board, guess, x, y):
 
-    #arr for column
-    column = [board[i][y] for i in range(9)]
-
-    #arr for square
-    tmpX = (x // 3) * 3
-    tmpY = (x // 3 ) * 3
-    square = []
-
-    for x in range (tmpX, tmpX + 3):
-        for y in range (tmpY, tmpY + 3):
-            square.append(board [x][y])
-
-    #checks row, column, & box
-    if guess in board[x]:
+    row_vals = board[x]
+    if guess in row_vals:
         return False
-    elif guess in column:
+
+    col_vals = [board[i][y] for i in range(9)]
+    if guess in col_vals:
         return False
-    elif guess in square:
-        return False
-    else:
-        return True
+
+    rows = (x // 3) * 3
+    columns = (y // 3) * 3
+
+    for r in range(rows, rows + 3):
+        for c in range(columns, columns + 3):
+            if board[r][c] == guess:
+                return False
+
+    return True
 
 
 
@@ -45,7 +41,7 @@ def solve(board):
         return True
     
     for guess in range(1,10):
-        if (isValid(board, x, y, guess)):
+        if (isValid(board, guess, x, y)):
             board[x][y] = guess
 
             if solve(board):
@@ -54,3 +50,22 @@ def solve(board):
         board[x][y] = -1
 
     return False
+
+
+
+if __name__ == '__main__':
+    example_board = [
+        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
+        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
+        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
+
+        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
+        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
+        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
+
+        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
+        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
+        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
+    ]
+    print(solve(example_board))
+    print(example_board)
